96f5a7ec108db928f467f7be2075ad529c6647da
Xiyan <xixiyannnn@gmail.com>
2023-03-08 17:54:24 +0800

    app pet

--- app_pet/src/main/java/com/example/app_pet/MainActivity.java
+++ app_pet/src/main/java/com/example/app_pet/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.app_pet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupActionBarWithNavController(this,navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        return navController.navigateUp();
     }
 }
